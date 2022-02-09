6c0cdaf81db6d0cfc463cc5aa3c7645ed5300a32
amruta-nejkar <amruta.nejkar@successive.tech>
2022-02-09 14:41:27 +0530

    counter update

--- src/Component/selfcounter.js
+++ src/Component/selfcounter.js
@@ -2,32 +2,33 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 const SelfCounter =()=>{
-  const [count,setCount] =useState(0);
-  const Updater =()=>{
-      let initialcount= 0;
-      // let x=20;
-      // let y=0;
-      const interval = setInterval(()=>{
-          if(initialcount<20){
-            initialcount= initialcount+1;
-          }
-          else{
-              //  y++;
-              initialcount=0;
-               clearInterval(interval);
-
-          }
-
-          setCount(initialcount);
-      },1000)
+    const [count,setCount] =useState(0);
+    const Updater =()=>{
+        let initialcount= count;
+        let forword=true;
+     setInterval(()=>{
+            if(forword){
+                if(initialcount!==20 ){
+                    initialcount= initialcount+1;
+                  }else{
+                    forword=false;
+                  }
+            }else if(!forword){
+                if(initialcount>0){
+                    initialcount= initialcount-1;      
+             }     
+             }
+            setCount(initialcount);
+        },1000)
+    }
+    useEffect(()=>{
+        Updater();
+    },[])
+    return(
+        <>
+            <h1>Counts: {count} </h1>
+        </>
+    )
   }
-  useEffect(()=>{
-      Updater();
-  },[])
-  return(
-      <>
-          <h1>Counts: {count} </h1>
-      </>
-  )
-}
-export default SelfCounter;
+
+  export default SelfCounter;
--- src/Module/header.js
+++ src/Module/header.js
@@ -1,13 +1,12 @@
 import * as React from 'react';
-import { useState } from 'react';
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
-import TextField from '@mui/material/TextField';
-import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
-import axios from "axios";
+import Search from '../Module/index';
+
+
 
 const Ul = styled.ul`
    display: flex;
@@ -22,21 +21,15 @@ const Li = styled.li`
  margin:10px;
  color:white;
 `
-function getData() {
-  axios
-    .get("http://localhost:3001/users")
-    .then((response) => {
-      getData(response.data);
-    })
-}
-
-const filter = createFilterOptions();
 const Header = () => {
-  const removeSession=()=>{
+ 
+  const removeSession = () => {
     localStorage.removeItem("getToken")
   }
-  const [value, setValue] = React.useState(null);
-  const [data, setData] = useState([]);
+
+
+
+
   return (
     <>
       <AppBar position="static">
@@ -51,67 +44,13 @@ const Header = () => {
               <Li><Link to="/modal">Modal</Link></Li>
               <Li><Link to="/loginuser">MUI-Login</Link></Li>
               <Li><Link to="/registration" onClick={removeSession}>Logout</Link></Li>
-              <Autocomplete
-                value={value}
-                onChange={(event, newValue) => {
-                  if (typeof newValue === 'string') {
-                    setValue({
-                      title: newValue,
-                    });
-                  } else if (newValue && newValue.inputValue) {
-                    // Create a new value from the user input
-                    setValue({
-                      title: newValue.inputValue,
-                    });
-                  } else {
-                    setValue(newValue);
-                  }
-                }}
-                filterOptions={(options, params) => {
-                  const filtered = filter(options, params);
-
-                  const { inputValue } = params;
-                  // Suggest the creation of a new value
-                  const isExisting = options.some((option) => inputValue === option.title);
-                  if (inputValue !== '' && !isExisting) {
-                    filtered.push({
-                      inputValue,
-                      title: `Add "${inputValue}"`,
-                    });
-                  }
-
-                  return filtered;
-                }}
-                selectOnFocus
-                clearOnBlur
-                handleHomeEndKeys
-                id="free-solo-with-text-demo"
-                options={getData}
-                getOptionLabel={(option) => {
-                  // Value selected with enter, right from the input
-                  if (typeof option === 'string') {
-                    return option;
-                  }
-                  // Add "xxx" option created dynamically
-                  if (option.inputValue) {
-                    return option.inputValue;
-                  }
-                  // Regular option
-                  return option.title;
-                }}
-                renderOption={(props, option) => <li {...props}>{option.title}</li>}
-                sx={{ width: 300 }}
-                freeSolo
-                renderInput={(params) => (
-                  <TextField {...params} label="" />
-                )}
-              />
-
-
-            </Ul>
+              <Li><Link to="/search">Search</Link></Li>
+</Ul>
           </Toolbar>
+
         </Container>
       </AppBar>
+     
 
 
     </>
